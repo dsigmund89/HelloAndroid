2fbc4a080d9f52787611f6ef7f1289ae437cef00
David Sigmund <davidsigmund89@gmail.com>
2014-02-05 16:03:02 -0600

    added another log message

--- HelloAndroid/src/com/example/helloandroid/MainActivity.java
+++ HelloAndroid/src/com/example/helloandroid/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         Log.d("tag","this is a test");
+        Log.d("tag", "hi");
     }
 
 
